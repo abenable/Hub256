147e7af4f0ab3c0f6cc6eb8795090aabd76fffd2
abenable <ableabenaitwe@gmail.com>
2024-04-24 03:02:12 +0300

    Update errorController.js to handle error details in ErrorHandler

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,20 +1,27 @@
 class ApiError extends Error {
-  constructor(statusCode, message) {
+  constructor(statusCode, message, details = null) {
     super(message);
     this.statusCode = statusCode;
+    this.details = details;
     this.name = this.constructor.name;
     this.isOperational = true;
     Error.captureStackTrace(this, this.constructor);
   }
 }
 
-const ErrorHandler = (err, req, res, next) => {
+const ErrorHandler = (err, req, res) => {
   let statusCode = err.statusCode || 500; // Internal Server Error code.
   let message = err.message || 'Internal Server Error';
+  let details = err.details;
+
+  if (process.env.NODE_ENV === 'development') {
+    console.error(err);
+  }
 
   res.status(statusCode).json({
     status: 'Error',
     error_message: message,
+    ...(details && { details: details }),
   });
 };
 
