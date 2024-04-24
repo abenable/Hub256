6c96801f1fd817570cc3d76aaabb015aa74085b2
abenable <ableabenaitwe@gmail.com>
2024-04-25 01:01:36 +0300

    Update logger configuration in logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,12 +1,18 @@
+import winstonTimestampColorize from 'winston-timestamp-colorize';
+import moment from 'moment';
+import path from 'path';
+
 import {
   createLogger,
   format as _format,
   transports as _transports,
 } from 'winston';
-import winstonTimestampColorize from 'winston-timestamp-colorize';
-import moment from 'moment';
+import { getDirname } from './util.js';
 
+const __dirname = getDirname(import.meta.url);
 const { combine, timestamp, colorize, printf, splat } = _format;
+
+const logFilePath = path.join(__dirname, '../logs', 'app.log'); // Specify the path to the log file
 
 const logger = createLogger({
   format: combine(
@@ -19,7 +25,10 @@ const logger = createLogger({
     printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
   ),
   level: 'debug',
-  transports: [new _transports.Console({})],
+  transports: [
+    new _transports.Console({}),
+    new _transports.File({ filename: logFilePath }), // Add the File transport
+  ],
 });
 
 export default logger;
