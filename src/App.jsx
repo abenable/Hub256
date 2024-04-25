d27a1ee88fa1908b25f546d3326fe578a086d374
abenable <ableabenaitwe@gmail.com>
2024-04-25 17:24:50 +0300

    Update authentication logic in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import BlogPage from './pages/blogPage/blogPage';
 import { Login } from './pages/login/login';
 import Footer from './components/footer/footer';
 import { ProtectedRoute } from './components/protectedRoute/protectedRoute';
-import { AuthProvider } from './components/auth/authProvider';
 import IndexPage from './pages/index';
 import Blogs from './pages/blogs/blogs';
 
@@ -39,67 +38,65 @@ function App() {
     return () => window.removeEventListener('scroll', toggleVisibility);
   }, []);
   return (
-    <AuthProvider>
-      <div className="App mx-4">
-        <Navbar />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <IndexPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="register" element={<Register />} />
-          <Route
-            path="newpost"
-            element={
-              <ProtectedRoute>
-                <CreatePost />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="blogs"
-            element={
-              <ProtectedRoute>
-                <Blogs />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="login" element={<Login />} />
-          <Route
-            path="blog/id/:postId"
-            element={
-              <ProtectedRoute>
-                <BlogPage />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>{' '}
-        <Footer />
-        {isVisible && (
-          <div
-            onClick={scrollToTop}
-            className="fixed bottom-2 right-2 cursor-pointer"
+    <div className="App mx-4">
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <IndexPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="register" element={<Register />} />
+        <Route
+          path="newpost"
+          element={
+            <ProtectedRoute>
+              <CreatePost />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="blogs"
+          element={
+            <ProtectedRoute>
+              <Blogs />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="login" element={<Login />} />
+        <Route
+          path="blog/id/:postId"
+          element={
+            <ProtectedRoute>
+              <BlogPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>{' '}
+      <Footer />
+      {isVisible && (
+        <div
+          onClick={scrollToTop}
+          className="fixed bottom-2 right-2 cursor-pointer"
+        >
+          <Button
+            size="sm"
+            className="p-2  bg-blue-gray-700 text-white rounded"
           >
-            <Button
-              size="sm"
-              className="p-2  bg-blue-gray-700 text-white rounded"
+            <svg
+              className="fill-white w-5 h-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
             >
-              <svg
-                className="fill-white w-5 h-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-              >
-                <path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"></path>
-              </svg>{' '}
-            </Button>
-          </div>
-        )}
-      </div>
-    </AuthProvider>
+              <path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"></path>
+            </svg>{' '}
+          </Button>
+        </div>
+      )}
+    </div>
   );
 }
 export default App;
