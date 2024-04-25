c25f9cd01166145fd8d0c4f8de75c4db1d1292d7
abenable <ableabenaitwe@gmail.com>
2024-04-25 17:24:12 +0300

    Add authentication redirect in ProtectedRoute component

--- src/components/protectedRoute/protectedRoute.jsx
+++ src/components/protectedRoute/protectedRoute.jsx
@@ -1,8 +1,17 @@
 import PropTypes from 'prop-types';
 import { useAuth } from '../auth/authProvider';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
   const isAuthenticated = useAuth();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate('/login');
+    }
+  }, [isAuthenticated, navigate]);
 
   return isAuthenticated ? children : null;
 }
