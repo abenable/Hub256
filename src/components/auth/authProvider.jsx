9befafb5b344aa027a5bee683d929ce83b7112cc
abenable <ableabenaitwe@gmail.com>
2024-04-25 15:00:06 +0300

    Add authentication API call in AuthProvider component

--- src/components/auth/authProvider.jsx
+++ src/components/auth/authProvider.jsx
@@ -6,22 +6,38 @@ const AuthContext = createContext();
 
 // Create a provider component that wraps your app and makes the authentication status available to all components
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 AuthProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
 export function AuthProvider({ children }) {
+  const API_URL =
+    import.meta.env.MODE === 'development'
+      ? import.meta.env.VITE_LOCAL_URL
+      : import.meta.env.VITE_URL;
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
-    const token = document.cookie.includes('jwt');
-    setIsAuthenticated(token);
-
-    if (!token) {
-      navigate('/login');
-    }
+    (async () => {
+      try {
+        const res = await axios.get(`${API_URL}/auth/checkauth`, {
+          withCredentials: true,
+        });
+        if (res.status === 200) {
+          setIsAuthenticated(true);
+        } else {
+          setIsAuthenticated(false);
+        }
+      } catch (error) {
+        setIsAuthenticated(false);
+      }
+      if (!isAuthenticated) {
+        navigate('/login');
+      }
+    })();
   }, [navigate]);
 
   return (
