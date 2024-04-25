bc86224d97022cf6b2b05b439cf8336e406b09a7
abenable <ableabenaitwe@gmail.com>
2024-04-25 17:24:41 +0300

    Update authentication logic in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { ThemeProvider } from "@material-tailwind/react";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { ThemeProvider } from '@material-tailwind/react';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './components/auth/authProvider.jsx';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
