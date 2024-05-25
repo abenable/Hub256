2e82cc2c79606171a5a99b8667efa8bd8dc6897d
abenable <ableabenaitwe@gmail.com>
2024-05-26 00:22:00 +0300

    chore: Update tailwind.config.js to include flowbite plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 import withMT from "@material-tailwind/react/utils/withMT";
+import flowbite from 'flowbite/plugin';
 export default withMT({
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./node_modules/flowbite/**/*.js", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbite],
 });
