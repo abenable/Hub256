4609dda0d8293a857d77804ea4698ec98e3020e8
abenable <ableabenaitwe@gmail.com>
2024-05-29 22:44:45 +0300

    refactor: Remove CheckAuth route and related logic

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import {
   AdminRegister,
-  CheckAuth,
   Login,
   Register,
   forgotpassword,
@@ -15,7 +14,6 @@ const router = express.Router();
 router.post('/register', Register);
 router.post('/admin/register', AdminRegister);
 router.post('/login', Login);
-router.get('/checkauth', CheckAuth);
 router.post('/forgotpassword', forgotpassword);
 router.post('/updatepassword', updatepassword);
 router.patch('/resetpassword', resetpassword);
